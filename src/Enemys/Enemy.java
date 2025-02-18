5e7f267b4e200a6efe3d30538b38747bedf839e5
Manhddm <manhducdongyb@gmail.com>
2025-02-19 00:29:27 +0700

    update lan 1

---
+++ src/Enemys/Enemy.java
@@ -0,0 +1,7 @@
+package Enemys;
+
+public class Enemy {
+    private String name;
+    private int hp;
+    private int damageAttack;
+}
--- src/Player.java
+++ src/Player.java
@@ -5,4 +5,32 @@ public class Player {
     private int exp;
     private int level;
     private int damageAttack;
+    private double x,y;//position
+    private double heSoExp = 1;
+    public Player(String name){
+        this.name = name;
+        this.hp = 100;
+        this.mp = 100;
+        this.exp = 0;
+        this.level = 1;
+        this.damageAttack = 10;
+        this.x = 0;
+        this.y = 0;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    //Level up
+
+    //Increase hp, mp, damageAttack
+
+    //Level up when exp >= 100
+
+    //Attack enemy
+
+    //Move player
+
+    //Apply item
+
+    //Get position
 }
